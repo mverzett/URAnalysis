e25e03b7a71ca100c4e3248ada2fe816b02e7902
Mauro Verzetti <mverzett@cern.ch>
2015-05-11 07:51:16 -0500

    more functionalities

--- Utilities/python/struct.py
+++ Utilities/python/struct.py
@@ -14,8 +14,28 @@ class Struct:
         newd.update(subs)
         return Struct(**newd)
 
+    def __len__(self):
+        return len(self.__dict__)
+
+    def __contains__(self, val):
+        return val in self.__dict__
+
     def __hash__(self):
         return self.__dict__.__repr__().__hash__()
+
+    def __getitem__(self, name):
+      'x.__getitem__(i, y) <==> x[i]'
+      return self.__dict__[name]
+
+    def __setitem__(self, name, val):
+      'x.__setitem__(i, y) <==> x[i]=y'
+      self.__dict__[name] = val
+
+    def iteritems(self):
+        return self.__dict__.iteritems()
+
+    def keys(self):
+        return self.__dict__.keys()
 
 class RecursiveStruct:
     def __init__(self, **entries):
