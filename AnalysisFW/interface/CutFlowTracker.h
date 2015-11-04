42473760778888c4b7a9f951382bceebce78c84e
mauro verzetti <mverzett@cern.ch>
2015-11-04 12:09:21 -0600

    use event weight if provided

--- AnalysisFW/interface/CutFlowTracker.h
+++ AnalysisFW/interface/CutFlowTracker.h
@@ -10,17 +10,21 @@ public:
 	CutFlowTracker():
 		cutflow_(),
 		npoints_(),
-		active_(true)	
+    active_(true),
+    verbose_(false)  
 	{}
 	void track(std::string pointname);
 	void writeTo(TFile &file);
 	void activate() {active_ = true;};
 	void deactivate() {active_ = false;};
+  void use_weight(float *w) {weight_ = w;}
+  void verbose(bool v) {verbose_ = v;}
 
 private:
-	std::map<std::string, std::pair<size_t, int> > cutflow_;
+	std::map<std::string, std::pair<size_t, float> > cutflow_;
 	size_t npoints_;
-	bool active_;
+	bool active_, verbose_;
+  float *weight_;
 };
 
 #endif
