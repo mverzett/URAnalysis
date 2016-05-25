8f40fcb54c745df0f78b072bfe909c6d6c48c7c4
mauro verzetti <mverzett@cern.ch>
2016-05-25 07:41:50 -0500

    move to argparse, like it shouls always have been

--- Utilities/scripts/dump_branch_names.py
+++ Utilities/scripts/dump_branch_names.py
@@ -5,20 +5,27 @@ The --type option prints together with the branch
 name also the C++ type for the branch
 '''
 
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('tfile')
+parser.add_argument('tree' )
+parser.add_argument('--type', '-t', action='store_true', help='dumps C++ type as well')
+args = parser.parse_args() 
+
 import sys
 if len(sys.argv) < 3 or '-h' in sys.argv or '--help' in sys.argv:
     print 'Usage dump_branch_names.py file.root path/to/Tree (--type, -t)'
     print __doc__
     sys.exit(1)
 
-print_type = '--type' in sys.argv or '-t' in sys.argv
+print_type = args.type
 #Open sample file
 import ROOT
 from pdb import set_trace
 ROOT.gROOT.SetBatch(True)
-tfile = ROOT.TFile.Open(sys.argv[-2])
+tfile = ROOT.TFile.Open(args.tfile)
 
-tree = tfile.Get(sys.argv[-1])
+tree = tfile.Get(args.tree)
 #Get All the branches
 if print_type:
     names = []
