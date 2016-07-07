f2ff1959878dd3fdc9018570bdde855fadc99008
mauro verzetti <mverzett@cern.ch>
2016-07-07 10:26:38 -0500

    add option for sub-cut flows

--- AnalysisFW/interface/CutFlowTracker.h
+++ AnalysisFW/interface/CutFlowTracker.h
@@ -13,17 +13,20 @@ public:
     active_(true),
     verbose_(false)  
 	{}
-	void track(std::string pointname);
-	void writeTo(TFile &file);
+	void track(std::string point, std::string groupname="");
+	void writeTo(TDirectory &file);
+	void writeTo(TDirectory &file, std::string group);
 	void activate() {active_ = true;};
 	void deactivate() {active_ = false;};
   void use_weight(float *w) {weight_ = w;}
   void verbose(bool v) {verbose_ = v;}
+	void group(std::string group) {group_ = group;}
 
 private:
 	std::map<std::string, std::pair<size_t, float> > cutflow_;
 	size_t npoints_;
 	bool active_, verbose_;
+	std::string group_;
   float *weight_;
 };
 
--- AnalysisFW/src/CutFlowTracker.cc
+++ AnalysisFW/src/CutFlowTracker.cc
@@ -1,9 +1,15 @@
 #include "URAnalysis/AnalysisFW/interface/CutFlowTracker.h"
 #include "URAnalysis/AnalysisFW/interface/Logger.h"
 #include "TH1F.h"
+#include <boost/algorithm/string/predicate.hpp>
+#include <tuple>
+#include <vector>
+#include <algorithm>
 
-void CutFlowTracker::track(std::string pointname)
+void CutFlowTracker::track(std::string label, std::string groupname)	
 {
+	if(groupname.size() == 0 && group_.size() != 0) groupname = group_;
+	std::string pointname = (groupname.size() != 0) ? groupname+"/"+label : label;
 	if(!active_) return;
   if(verbose_) Logger::log().debug() << pointname << std::endl;
   float to_add = (weight_) ? *weight_ : 1.;
@@ -25,7 +31,7 @@ void CutFlowTracker::track(std::string pointname)
 	}
 }
 
-void CutFlowTracker::writeTo(TFile &file)
+void CutFlowTracker::writeTo(TDirectory &file)
 {
 	file.cd();
 	TH1F histo("cut_flow", "cut_flow", npoints_, 0, npoints_);
@@ -33,6 +39,35 @@ void CutFlowTracker::writeTo(TFile &file)
 	for(auto& point : cutflow_){
 		xax->SetBinLabel(point.second.first+1, point.first.c_str());
 		histo.SetBinContent(point.second.first+1, point.second.second);
+	}
+	histo.Write();
+}
+
+void CutFlowTracker::writeTo(TDirectory &file, std::string group)
+{
+	file.cd();
+	size_t npts = 0;
+	std::vector< std::tuple<std::string, size_t, float> > bins;
+	for(auto& entry : cutflow_) {
+		if(boost::starts_with(entry.first, group)) {
+			npts++;
+			std::string name = entry.first.substr(group.size()+1,entry.first.size()-group.size()-1);
+			size_t idx = entry.second.first;
+			float  val = entry.second.second;
+			bins.push_back(
+				std::make_tuple(name, idx, val)
+				);			
+		}
+	}
+	std::sort(bins.begin(), bins.end(), [](auto const &t1, auto const &t2) { //black compiler magic!
+			return std::get<1>(t1) < std::get<1>(t2); 
+		});
+
+	TH1F histo("cut_flow", "cut_flow", npts, 0, npts);
+	TAxis *xax = histo.GetXaxis();
+	for(size_t i = 0; i < npts; ++i){
+		xax->SetBinLabel(i+1, std::get<0>(bins[i]).c_str());
+		histo.SetBinContent(i+1, std::get<2>(bins[i]));
 	}
 	histo.Write();
 }
